60908e1319678bad9c35c6e8af75772de5308b01
Artyom <artyomka13@gmail.com>
2022-02-16 15:27:00 +0300

    add ui elements
    - red, yellow and red lights (views)
    - start button

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -8,7 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // MARK: - Elements
+    private lazy var redLight: UIView = {
+        let redLight = UIView()
+        redLight.backgroundColor = .systemRed
+        redLight.translatesAutoresizingMaskIntoConstraints = false
+        return redLight
+    }()
 
+    private lazy var yellowLight: UIView = {
+        let yellowLight = UIView()
+        yellowLight.backgroundColor = .systemYellow
+        yellowLight.translatesAutoresizingMaskIntoConstraints = false
+        return yellowLight
+    }()
+
+    private lazy var greenLight: UIView = {
+        let greenLight = UIView()
+        greenLight.backgroundColor = .systemGreen
+        greenLight.translatesAutoresizingMaskIntoConstraints = false
+        return greenLight
+    }()
+
+    private lazy var lightsStackView: UIStackView = {
+        let lights = UIStackView()
+        lights.axis = .vertical
+        lights.spacing = 40
+        lights.translatesAutoresizingMaskIntoConstraints = false
+        return lights
+    }()
+
+    private lazy var startButton: UIButton = {
+        let button = UIButton()
+        button.layer.cornerRadius = 15
+        button.setTitle("START", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .black
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
