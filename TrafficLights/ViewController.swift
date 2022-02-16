86f38e8ee84f9a9c1a5c4317ed28011212651f6f
Artyom <artyomka13@gmail.com>
2022-02-16 16:04:26 +0300

    add start button target and metric

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -7,16 +7,12 @@
 
 import UIKit
 
-enum CurrentLight {
-    case red, yellow, green
-}
-
 class ViewController: UIViewController {
     // MARK: - Elements
     private lazy var redLight: UIView = {
         let redLight = UIView()
         redLight.backgroundColor = .systemRed
-        redLight.alpha = lightIsOff
+        redLight.alpha = Metric.lightIsOff
         redLight.translatesAutoresizingMaskIntoConstraints = false
         return redLight
     }()
@@ -24,7 +20,7 @@ class ViewController: UIViewController {
     private lazy var yellowLight: UIView = {
         let yellowLight = UIView()
         yellowLight.backgroundColor = .systemYellow
-        yellowLight.alpha = lightIsOff
+        yellowLight.alpha = Metric.lightIsOff
         yellowLight.translatesAutoresizingMaskIntoConstraints = false
         return yellowLight
     }()
@@ -32,7 +28,7 @@ class ViewController: UIViewController {
     private lazy var greenLight: UIView = {
         let greenLight = UIView()
         greenLight.backgroundColor = .systemGreen
-        greenLight.alpha = lightIsOff
+        greenLight.alpha = Metric.lightIsOff
         greenLight.translatesAutoresizingMaskIntoConstraints = false
         return greenLight
     }()
@@ -40,14 +36,14 @@ class ViewController: UIViewController {
     private lazy var lightsStackView: UIStackView = {
         let lights = UIStackView()
         lights.axis = .vertical
-        lights.spacing = 40
+        lights.spacing = Metric.lightsStackViewSpacing
         lights.translatesAutoresizingMaskIntoConstraints = false
         return lights
     }()
 
     private lazy var startButton: UIButton = {
         let button = UIButton()
-        button.layer.cornerRadius = 15
+        button.layer.cornerRadius = Metric.startButtonCornerRadius
         button.setTitle("START", for: .normal)
         button.setTitleColor(.white, for: .normal)
         button.backgroundColor = .black
@@ -55,9 +51,11 @@ class ViewController: UIViewController {
         return button
     }()
     
+    enum CurrentLight {
+        case red, yellow, green
+    }
+    
     private var currentLight = CurrentLight.red
-    private let lightIsOn: CGFloat = 1
-    private let lightIsOff: CGFloat = 0.3
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
@@ -73,8 +71,29 @@ class ViewController: UIViewController {
         yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
         greenLight.layer.cornerRadius = greenLight.frame.width / 2
     }
+    
+    // MARK: - Actions
+    @objc func startButtonPressed() {
+        startButton.setTitle("NEXT", for: .normal)
+
+        switch currentLight {
+        case .red:
+            redLight.alpha = Metric.lightIsOn
+            greenLight.alpha = Metric.lightIsOff
+            currentLight = .yellow
+        case .yellow:
+            redLight.alpha = Metric.lightIsOff
+            yellowLight.alpha = Metric.lightIsOn
+            currentLight = .green
+        case .green:
+            yellowLight.alpha = Metric.lightIsOff
+            greenLight.alpha = Metric.lightIsOn
+            currentLight = .red
+        }
+    }
 }
 
+// MARK: - Settings
 extension ViewController {
     private func setupHierarch() {
         view.addSubview(lightsStackView)
@@ -84,30 +103,53 @@ extension ViewController {
         lightsStackView.addArrangedSubview(yellowLight)
         lightsStackView.addArrangedSubview(greenLight)
         
+        startButton.addTarget(self, action: #selector(startButtonPressed), for: .touchUpInside)
     }
     
     private func setupLayout() {
         NSLayoutConstraint.activate([
-            redLight.widthAnchor.constraint(equalToConstant: 100),
-            redLight.heightAnchor.constraint(equalToConstant: 100),
+            redLight.widthAnchor.constraint(equalToConstant: Metric.redLightWidth),
+            redLight.heightAnchor.constraint(equalToConstant: Metric.greenLightHeight),
 
-            yellowLight.widthAnchor.constraint(equalToConstant: 100),
-            yellowLight.heightAnchor.constraint(equalToConstant: 100),
+            yellowLight.widthAnchor.constraint(equalToConstant: Metric.yellowLightWidth),
+            yellowLight.heightAnchor.constraint(equalToConstant: Metric.yellowLightHeight),
 
-            greenLight.widthAnchor.constraint(equalToConstant: 100),
-            greenLight.heightAnchor.constraint(equalToConstant: 100),
+            greenLight.widthAnchor.constraint(equalToConstant: Metric.greenLightWidth),
+                                              greenLight.heightAnchor.constraint(equalToConstant: Metric.greenLightHeight),
 
-            lightsStackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            lightsStackView.topAnchor.constraint(equalTo: view.topAnchor, constant: Metric.lightsStackViewTopOfSet),
             lightsStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
 
             startButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            startButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50),
-            startButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
-            startButton.heightAnchor.constraint(equalToConstant: 60)
+            startButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: Metric.startButtonBottomOfSet),
+            startButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: Metric.startButtonWidthMultiplier),
+            startButton.heightAnchor.constraint(equalToConstant: Metric.startButtonHeight)
         ])
     }
     
     private func setupView() {
         view.backgroundColor = .systemGray3
+    }
+}
+
+extension ViewController {
+    enum Metric {
+        static let redLightWidth: CGFloat = 100
+        static let redLightHeight: CGFloat = 100
+        static let yellowLightWidth: CGFloat = 100
+        static let yellowLightHeight: CGFloat = 100
+        static let greenLightWidth: CGFloat = 100
+        static let greenLightHeight: CGFloat = 100
+        
+        static let lightsStackViewTopOfSet: CGFloat = 100
+        static let lightsStackViewSpacing: CGFloat = 40
+        
+        static let startButtonBottomOfSet: CGFloat = -50
+        static let startButtonWidthMultiplier: CGFloat = 0.8
+        static let startButtonHeight: CGFloat = 60
+        static let startButtonCornerRadius: CGFloat = 15
+        
+        static let lightIsOn: CGFloat = 1
+        static let lightIsOff: CGFloat = 0.3
     }
 }
